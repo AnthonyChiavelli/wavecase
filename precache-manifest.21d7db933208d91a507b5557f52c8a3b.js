1ecf20e42a435dda049074493c5515d341b964db
Anthony Reid <anthony@Anthonys-MacBook-Pro.local>
2018-11-12 00:43:08 -0500

    Updates

---
+++ precache-manifest.21d7db933208d91a507b5557f52c8a3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aae2030ca137ae7e4804",
+    "url": "/wavecase/static/css/main.64462b6d.chunk.css"
+  },
+  {
+    "revision": "aae2030ca137ae7e4804",
+    "url": "/wavecase/static/js/main.aae2030c.chunk.js"
+  },
+  {
+    "revision": "daf10da62f086e8ef9e1",
+    "url": "/wavecase/static/css/1.679eeb6e.chunk.css"
+  },
+  {
+    "revision": "daf10da62f086e8ef9e1",
+    "url": "/wavecase/static/js/1.daf10da6.chunk.js"
+  },
+  {
+    "revision": "36216de332df2ddc8681",
+    "url": "/wavecase/static/js/runtime~main.36216de3.js"
+  },
+  {
+    "revision": "f96a6b6b387e09ad58127b1efe1f31d2",
+    "url": "/wavecase/index.html"
+  }
+];
